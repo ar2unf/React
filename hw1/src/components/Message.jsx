47186de6d340ee0fcbc4421d0b7fca5e186b5d79
ar2unf <dim960@gmail.com>
2019-10-21 22:51:28 +0300

    HW2_welldone

--- hw1/src/components/Message.jsx
+++ hw1/src/components/Message.jsx
@@ -5,9 +5,10 @@ import PropTypes from 'prop-types';
 export default class Message extends React.Component {
    static propTypes = {
        text: PropTypes.string.isRequired,
+       senderId:PropTypes.string.isRequired,
    };
 
    render() {
-       return <div>{ this.props.text }</div>
+       return <div>{ this.props.senderId }: { this.props.text }</div>
    }
 }
--- hw1/src/components/MessageField.jsx
+++ hw1/src/components/MessageField.jsx
@@ -1,22 +1,50 @@
 import React from 'react';
 import Message from './Message.jsx';
 
+const BOT_ANSWER =['Не мешай','Отойди','Кожаный мешок','Уйди!','анигилирую!','Завязывай!'];
+
+
+const GetRandomBotAnswer = ()=>BOT_ANSWER[Math.floor(Math.random()*BOT_ANSWER.length)];
+
+
 export default class MessageField extends React.Component {
    state = {
-       messages: ["Привет!", "Как дела?"]
+      // messages: ["Привет!", "Как дела?"]
+      messages:[
+          {
+              senderId :"AngryBot",
+              text: "Привет!"
+          },
+          {
+            senderId :"AngryBot",
+            text: "Кожаный Мешок"
+        },
+
+      ]
    };
 
-   handleClick = () => {
-       this.setState({ messages: [ ...this.state.messages, 'Нормально' ] });
+   componentDidUpdate() {
+    if (this.state.messages[this.state.messages.length-1].senderId=="human") {  
+        setTimeout(() =>
+        this.setState(
+            { messages: [ ...this.state.messages, {senderId:"AngryBot",text:GetRandomBotAnswer()} ] }),
+        200);
+    }
+}
+
+
+   handleSendMesage = () => {
+       this.setState({ messages: [ ...this.state.messages, {senderId:"human",text:'Нормально'} ] });
    };
 
    render() {
-       const messageElements = this.state.messages.map((text, index) => (
-           <Message key={ index } text={ text } />));
+       const messageElements = this.state.messages.map((message, index) => (
+           <Message key={ index } text={ message.text } senderId ={message.senderId} />));
 
        return <div>
-           { messageElements }
-           <button onClick={ this.handleClick }>Отправить сообщение</button>
+       <h1>Чат</h1>
+           <div >{ messageElements }</div>
+           <button onClick={ this.handleSendMesage }>Отправить сообщение</button>
        </div>
    }
 }
