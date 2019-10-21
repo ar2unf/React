43bdbe1721b33c8f930b4b75e963b7d393e86f64
ar2unf <dim960@gmail.com>
2019-10-21 20:24:53 +0300

    hw1
    done

--- hw1/src/index.js
+++ hw1/src/index.js
@@ -5,11 +5,26 @@ import ReactDOM from 'react-dom';
 
 let messages = ['Привет', 'Как дела?'];
 
+const sendMessage = ()=>{
+   messages.push('Нормально');
+   ReactDOM.render(
+   <MessageField messages={ messages } />,
+   document.getElementById('root'),
+);
+
+}
+
 const MessageComponent = (props) => <div>{props.text}</div>;
 
-const MessageField = (props) => {
-   return props.messages.map(message => <MessageComponent text={ message } />);
-};
+const MessageField = (props) => 
+   <div>
+      <h1>Чат</h1>
+      {props.messages.map(message => <MessageComponent text={ message } />)}
+      <button onClick={sendMessage}>Добавить сообщения</button>
+   </div>
+
+  
+
 
 ReactDOM.render(
    <MessageField messages={ messages } />,
